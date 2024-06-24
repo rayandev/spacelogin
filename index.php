1c6bbe93d41d1b997e0fc711e6cbb289ad868896
Rayan Jaberi <rayanjaberi2001@gmail.com>
2024-06-24 19:18:20 +0330

    d-none desk section and add mobile section with img

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/main.css">
 </head>
+
 <body>
     <form action="" method="post" id="Main_form">
         <section class="form_content">
@@ -21,5 +23,10 @@
         </section>
         <section class="form_img"><img src="./img/rock.jpg" alt=""></section>
     </form>
+
+    <form action="" method="post" id="main-form-mobile">
+        <section class="form-img-mobile"><img src="./img/rock.jpg" alt=""></section>
+    </form>
 </body>
+
 </html>
